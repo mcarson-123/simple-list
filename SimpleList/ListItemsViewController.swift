ffb939b33f4adb1aee778ef2bdb61ea81b07594c
Madeline Carson <madeline.carson@gmail.com>
2016-03-20 08:53:20 -0700

    Fix overlapping status bar

--- SimpleList/ListItemsViewController.swift
+++ SimpleList/ListItemsViewController.swift
@@ -11,6 +11,9 @@ class ListItemsViewController: UITableViewController {
         return true
     }
     
+    // TODO: create multiple sections
+    // 1 section for input
+    // 1 section for the created list items
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
     }
@@ -20,5 +23,14 @@ class ListItemsViewController: UITableViewController {
         let cell = tableView.dequeueReusableCellWithIdentifier("TextInputCell") as! TextInputTableViewCell
         
         return cell
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let statusBarHeight = UIApplication.sharedApplication().statusBarFrame.height
+        let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
+        tableView.contentInset = insets
+        tableView.scrollIndicatorInsets = insets
     }
 }
